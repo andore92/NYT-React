a7e78fdb3ff864bf2052253ed71767e01fa73d4b
Andrew Sadowski <Andrew Sadowski>
2017-08-16 18:00:55 -0400

    set up server boilerplate code and package.json file

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var Article = require('./models/Article.js');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Run Morgan for Logging
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+app.use(express.static('./public'));
+
+mongoose.connect('mongodb://localhost/nytreact');
+
+
+
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+  console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function () {
+  console.log('Mongoose connection successful.');
+});
+
+app.get('/', function(req, res){
+  res.sendFile('./public/index.html');
+})
